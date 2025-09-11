060f951e25db10301be16878848bc5aad845eb2d
Ahmed-Rafi07 <rafi62224@gmail.com>
2025-09-11 12:16:35 +0530

    Conneting to DB

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
-const { resolve } = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
-const port = 3010;
 
-app.use(express.static('static'));
+// Midlware (to parse JSON requests)
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+   })
+.then(()=>{
+  console.log('Connected to MongoDB');
+})
+.catch((err)=>{
+  console.log(`Error connecting to MongoDB ${err}`)
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.get("/",(req,res)=> {
+  res.send("Student Project Tracker Backend is running")
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
